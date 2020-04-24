d657097baf47fa245d5099c281480248e1529e6f
MMrFalcon <kubasokol123456@gmail.com>
2020-04-24 17:42:37 +0200

    Adding test profile +  CI maven tests customization

--- src/test/java/com/falcon/movies/MoviesApplicationTests.java
+++ src/test/java/com/falcon/movies/MoviesApplicationTests.java
@@ -2,8 +2,10 @@ package com.falcon.movies;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MoviesApplicationTests {
 
 	@Test
