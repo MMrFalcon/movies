da7da80f8d4ca6dffed92daebcdf6ea11d47f244
MMrFalcon <kubasokol123456@gmail.com>
2021-03-30 20:47:36 +0200

    Adding abstract class for header response.

--- src/main/java/com/falcon/movies/web/controller/MovieController.java
+++ src/main/java/com/falcon/movies/web/controller/MovieController.java
@@ -37,34 +37,8 @@ public class MovieController {
     public ResponseEntity<List<MovieDto>> getByCriteria(MovieCriteria movieCriteria, Pageable pageable) {
         log.debug("Rest request for get Movies by criteria : {} ", movieCriteria);
         Page<MovieDto> moviesPage = movieQueryService.findByCriteria(movieCriteria, pageable);
-        HttpHeaders headers = createGetResponseHeaders(moviesPage);
+        HttpHeaders headers = ResponseHeaderGenerator.createGetResponseHeaders(moviesPage);
         return ResponseEntity.ok().headers(headers).body(moviesPage.getContent());
     }
 
-    // TODO add abstract class
-    protected HttpHeaders createGetResponseHeaders(Page<MovieDto> page) {
-        final int pageNumber = page.getNumber();
-        final int pageSize = page.getSize();
-        final int nextPage = pageNumber + 1;
-
-        int prevPage = pageNumber - 1;
-        if (prevPage < 0) prevPage = 0;
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("M-Page-Size", String.valueOf(pageSize));
-        headers.add("M-Page-Count", String.valueOf(page.getTotalPages()));
-        headers.add("M-Page-Number", String.valueOf(pageNumber));
-        headers.add("M-Total-Count", String.valueOf(page.getTotalElements()));
-        headers.add("M-Next-Page", getUriByPage(nextPage, pageSize));
-        headers.add("M-Prev-Page", getUriByPage(prevPage, pageSize));
-        return headers;
-    }
-
-    protected String getUriByPage(int pageNumber, int pageSize) {
-        final UriComponentsBuilder uriComponentsBuilder = ServletUriComponentsBuilder.fromCurrentRequest();
-        return uriComponentsBuilder
-                .replaceQueryParam("page", Integer.toString(pageNumber))
-                .replaceQueryParam("size", Integer.toString(pageSize))
-                .toUriString();
-    }
 }
