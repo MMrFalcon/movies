afbcfa36f8b8251063eabea3b705c90ccf33a289
MMrFalcon <kubasokol123456@gmail.com>
2022-01-22 16:40:57 +0100

    Replacing affected H2 database with HSQL db + spring version upgrade to 2.6.3.

--- src/main/java/com/falcon/movies/MoviesApplication.java
+++ src/main/java/com/falcon/movies/MoviesApplication.java
@@ -2,8 +2,10 @@ package com.falcon.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.falcon.movies.entity")
 public class MoviesApplication {
 
 	public static void main(String[] args) {
